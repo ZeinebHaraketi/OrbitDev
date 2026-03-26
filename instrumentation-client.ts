dbb05214dcc3f865b14cf75e30a3e4e9e4040c46
Zeineb Haraketi <zeinebharaketi298@gmail.com>
2026-03-26 23:55:04 +0000

    some fixes with codeRabbit

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,10 +1,9 @@
-import posthog from 'posthog-js'
+import posthog from "posthog-js"
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  api_host: "/ingest",
+  ui_host: "https://us.posthog.com",
   defaults: '2025-05-24',
-})
-
-export function onRouterTransitionStart(url: string) {
-  posthog.capture('$pageview', { $current_url: url })
-}
+  capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+  debug: process.env.NODE_ENV === "development",
+});
